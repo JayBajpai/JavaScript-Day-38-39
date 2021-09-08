740896d7d6eff03e7c8e14bba1273db43a67c64f
Jay Bajpai <52155590+JayBajpai@users.noreply.github.com>
2021-09-08 09:44:37 +0530

    DayWithDailyWageUsingMapHelperFunction

---
+++ EmpWageUC7&7B.js
@@ -0,0 +1,49 @@
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const WAGE_PER_HR = 20;
+
+function getEmpWorkingHrs(empCheck) {
+    switch(empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HRS;
+    
+        case IS_FULL_TIME:
+            return FULL_TIME_HRS;
+    
+            default:
+                return 0;
+    }
+}
+
+function calcEmpDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HR;
+}
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+        totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+            let empCheck = Math.floor(Math.random() * 10) % 3;
+            let empHrs = getEmpWorkingHrs(empCheck);
+            totalEmpHrs += empHrs;
+            empDailyWageArr.push(calcEmpDailyWage(empHrs));
+}
+let empWage = calcEmpDailyWage(totalEmpHrs);
+console.log("Total Working Days: "+totalWorkingDays +" Total Working Hours: "+totalEmpHrs +" Employee Wage: "+empWage);
+
+//UC 7b - show the days along eith Daily Wage using array map helper function
+let dailyCntr = 0;
+function mapDayWithWage(dailyWage) {
+    dailyCntr++;
+    return dailyCntr + " = " + dailyWage;
+}
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("Daily Wage Map");
+console.log(mapDayWithWageArr);
